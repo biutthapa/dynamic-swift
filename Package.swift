38ad4ee7b1546209d98221cc187a38c91a1c96d3
Biut Thapa <mailtobiut@gmail.com>
2024-08-30 03:47:40 +0200

    Dynamo

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "doodle-swift",
+    name: "dynamo",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "DoodleSwift", targets: ["DoodleSwift"]),
+        .library(name: "Dynamo", targets: ["Dynamo"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-		.target(name: "DoodleSwift", dependencies: []),
-//        .executableTarget(name: "DoodleSwiftREPL", dependencies: ["DoodleSwift"]),
+		.target(name: "Dynamo", dependencies: []),
+//        .executableTarget(name: "DynamoREPL", dependencies: ["Dynamo"]),
     ]
 )
